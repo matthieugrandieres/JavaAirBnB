2e4317abb68b0697a6d80e3f817045d42f85f70e
matthieugrandieres <66612277+matthieugrandieres@users.noreply.github.com>
2021-01-07 14:07:00 +0100

    Update Reservation.java

--- airbnb/reservations/Reservation.java
+++ airbnb/reservations/Reservation.java
@@ -2,6 +2,7 @@ package matthieu.airbnb.reservations;
 import matthieu.airbnb.utilisateurs.Personne;
 import matthieu.airbnb.logements.Logement;
 import matthieu.airbnb.utilisateurs.Voyageur;
+import matthieu.airbnb.utilisateurs.Hote;
 
 import java.util.Date;
 
@@ -12,21 +13,25 @@ public class Reservation {
     private Voyageur voyageur;
     private boolean estValidee;
     private String dateDeReservation;
+    private Hote hote;
 
-    public Reservation(int pId, Sejour pSejour, Voyageur pVoyageur, boolean pEstValidee, String pDate){
+    public Reservation(int pId, Sejour pSejour, Voyageur pVoyageur, Hote pHote, boolean pEstValidee, String pDate){
         id = pId;
         sejour = pSejour;
         voyageur = pVoyageur;
         estValidee = pEstValidee;
         dateDeReservation = pDate;
+        hote = pHote;
     }
 
     public void afficher(){
-        System.out.println(voyageur.getPrenom() + " " + voyageur.getNom() + " (" + voyageur.getAge() + " ans)");
+        System.out.println(voyageur.getPrenom() + " " + voyageur.getNom() + " (" + voyageur.getAge() + " ans) " +
+                " a fait une reservation chez "  + hote.getNom() + " " + hote.getPrenom() + " " + hote.getAge());
+        System.out.println("Qui s engage à répondre dans les " + hote.getDelaiDeReponse() + " heures");
         System.out.println("Le logement est situé : " + sejour.getLogementAdresse());
         System.out.println("Superficie : " + sejour.getLogementSuperficie() + "m2");
         System.out.println("La date d'arrivée est le : " + dateDeReservation + " pour " + sejour.getNbNuits() + " nuits");
-        System.out.println("Le prix du séjour est de " + sejour.getNbNuits() * sejour.getLogementTarif());
+        System.out.println("Le prix du séjour est de " + sejour.getNbNuits() * sejour.getLogementTarif() + " euros");
     }
 
 
