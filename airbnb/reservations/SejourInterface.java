9e1889b1154b3152bb4bbd6b8ba9a490c00760fb
matthieugrandieres <66612277+matthieugrandieres@users.noreply.github.com>
2021-01-07 14:21:09 +0100

    Update SejourInterface.java

--- airbnb/reservations/SejourInterface.java
+++ airbnb/reservations/SejourInterface.java
@@ -1,23 +1,38 @@
 package matthieu.airbnb.reservations;
 
+import java.util.Date;
+
 public interface SejourInterface {
 
-    public default boolean verificationDateArrive(){
-        return false;
+    public default boolean verificationDateArrive(Date pDateArrivee, Date pDateNow){
+        if (pDateArrivee.compareTo(pDateNow) > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public default boolean verificationNbNuits(){
-        return false;
+    public default boolean verificationNbNuits(int pNbNuits){
+        int min = 1;
+        int max = 31;
+
+        if (pNbNuits > min && pNbNuits < max ){
+            return true;
+        } else {
+            return false;
+        }
     }
-    public default boolean verificationNbVoyageurs(){
-        return false;
+    public default boolean verificationNbVoyageurs(int pNbVoyageursMax, int nbVoyageurs){
+        if (pNbVoyageursMax > nbVoyageurs){
+            return false;
+        } else {
+            return true;
+        }
     }
 
     public default void afficher(){
 
     }
-
-
 
 
 }
